f932dfeaba6fc57a05096befdc60a373739f8dfb
wucl202000 <wucl-20@163.com>
2016-07-25 16:30:57 +0800

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+# Platform:                Linux
+# Python:                  3.5
+# Author:                  wucl(wucl-20@163.com)
+# History:                 2016.7.25
+
+print("Hello, World!")
